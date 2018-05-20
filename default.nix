6ea7030139577690820e31265356076ec03dd2b0
Michael Hudson <huddo121@gmail.com>
2018-05-20 12:52:28 +1000

    Set up nix environment for the blog

---
+++ default.nix
@@ -0,0 +1,17 @@
+# Taken from http://stesie.github.io/2016/08/nixos-github-pages-env
+with import (fetchTarball https://nixos.org/channels/nixos-18.03/nixexprs.tar.xz) { };
+let jekyll_env = bundlerEnv rec {
+    name = "jekyll_env";
+    gemfile = ./Gemfile;
+    lockfile = ./Gemfile.lock;
+    gemset = ./gemset.nix;
+  };
+in
+  stdenv.mkDerivation rec {
+    name = "jekyll_env";
+    buildInputs = [ jekyll_env ];
+
+    shellHook = ''
+      exec ${jekyll_env}/bin/jekyll serve --watch
+    '';
+  }
